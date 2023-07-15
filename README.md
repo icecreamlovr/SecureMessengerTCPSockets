aa72db4e196d61bc137e054827768a733867a6b3
Yue He <yuehe.neu@gmail.com>
2023-07-15 01:37:59 -0700

    Add skeleton socket client and server. Server creates thread for each new client connection.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Secure-Messenger---TCP-Sockets
+# Secure Messenger based on TCP Sockets
+
+A secure messaging system with CLI interface. The system is implemented in C and based on TCP sockets. It consists of a centralized server (server/message_server.c) and many clients (client/message_client.c). The system employs End-to-End encryption between any clients for max level of security.
+
+### To run the program:
+
+Start one instance of server:
+```bash
+$ gcc src/server/message_server.c -o server.o
+$ ./server.o
+```
+
+Start two instances of client:
+```bash
+# On another computer, or another terminal window on localhost
+$ gcc src/client/message_client.c -o client.o
+$ ./client.o
+
+# On a third computer, or a third terminal window on localhost
+$ gcc src/client/client.c -o client.o
+$ ./client.o
+```
---
+++ src/client/message_client.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define MAX_MESSAGE_LENGTH 1024
+#define SERVER_ADDRESS "127.0.0.1"
+#define SERVER_PORT 12345
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_address;
+    char message[MAX_MESSAGE_LENGTH];
+
+    // Create a socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Error: Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prepare the server address structure
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    server_address.sin_port = htons(SERVER_PORT);
+
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        perror("CError: onnection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully connected to the server.\n");
+
+    while (1) {
+        // Get a message from the user
+        printf("> ");
+        fgets(message, sizeof(message), stdin);
+
+        // Remove the trailing newline character
+        message[strcspn(message, "\n")] = '\0';
+
+        // Send the message to the server
+        if (send(client_socket, message, strlen(message), 0) == -1) {
+            perror("Error: Message sending failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Receive a response from the server
+        memset(message, 0, sizeof(message));
+        ssize_t bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received == -1) {
+            perror("Error: Response receiving failed");
+            exit(EXIT_FAILURE);
+        }
+        if (bytes_received == 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+
+        printf("Received from server: %s\n", message);
+    }
+
+    // Close the socket
+    close(client_socket);
+
+    return 0;
+}
